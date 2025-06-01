1ae2c5e1c3af2d2762c11c36f106dd42273b4e34
David Camacho <ing.dc.pro@gmail.com>
2025-06-01 10:40:01 -0700

    modification practice 1

--- suma_1.py
+++ suma_1.py
@@ -1,5 +1,5 @@
-nuemro_1 = float(input("ingresa el primer numero:  "))
-nuemro_2 = float(input("ingresa el segundo numero:  "))
+numero_1 = float(input("ingresa el primer numero:  "))
+numero_2 = float(input("ingresa el segundo numero:  "))
 
 suma = nuemro_1 + nuemro_2
 
