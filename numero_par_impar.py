d4bc2deb4bf31f2d07b2b7a9d656d4c19e2ce7eb
David Camacho <ing.dc.pro@gmail.com>
2025-06-01 23:22:10 -0700

    pracice 2

---
+++ numero_par_impar.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingresa un numero entero: "))
+
+if numero  == 0:
+    print("El numero no es par ni tampoco impar")
+elif numero % 2 == 0:
+    print("El numero es par")
+else:
+    print("El Numero es impar")
