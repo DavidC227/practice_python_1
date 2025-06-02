0803413bd7caf45bae76ce808dac7b22e4c43767
David Camacho <ing.dc.pro@gmail.com>
2025-06-01 23:36:55 -0700

    practice 3

---
+++ main.py
@@ -0,0 +1,9 @@
+# Paso 1 y 2: Pedimos al usuario los números
+numero1 = float(input("Escribe el primer número: "))
+numero2 = float(input("Escribe el segundo número: "))
+
+# Paso 3: Sumamos los números
+suma = numero1 + numero2
+
+# Paso 4: Mostramos el resultado
+print("La suma es:", suma)
