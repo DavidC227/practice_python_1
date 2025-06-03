962033a6b731b25c812e08ba06a6b5cfb43cd6a8
David Camacho <ing.dc.pro@gmail.com>
2025-06-03 00:13:20 -0700

    practice 4

---
+++ sumar_lista.py
@@ -0,0 +1,12 @@
+
+lista_num = [0,1,2,3,4,5,6,7,8,9]
+suma = 0
+
+for i in lista_num:
+    suma += i
+    print(i,suma)
+print()
+print("La suma total de los numeros de la lista es:",suma)
+print()
+
+
