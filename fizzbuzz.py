46fe1bc25494c19533528240c47f20d12bc9ba04
David Camacho <ing.dc.pro@gmail.com>
2025-06-09 14:53:38 -0700

    technical test 1

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+# mostrar los numeros del 1 -100 
+
+def fizzbuzz():
+
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print(i,"fizzbuzz")
+        elif i % 3 == 0:
+            print(i,"fizz")
+        elif i % 5 == 0:
+            print(i,"buzz")
+        else:
+            print(i)
+
+fizzbuzz()
+
