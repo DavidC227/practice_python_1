459a54396a084e8843e8f053bc1bc9dfe207cbae
David Camacho <ing.dc.pro@gmail.com>
2025-06-10 16:58:24 -0700

    input for to a list P17

---
+++ input_for_to_list.py
@@ -0,0 +1,6 @@
+start = int(input("Ingresa el primer numero de la lista: "))
+end = int(input("Ingresa el ultimo numero de la lista: "))
+
+list_1 = [i for i in range(start,end +1 )]
+
+print(list_1)
