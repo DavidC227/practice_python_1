c04049443fae192d5c9e07fd5517432082d420ab
David Camacho <ing.dc.pro@gmail.com>
2025-06-13 14:57:18 -0700

    Avarage, len, sum, min, max P24

---
+++ avg_max_min.py
@@ -0,0 +1,7 @@
+nums = [7, 4, 9, 10, 5, 3, 6] 
+
+avarage = round(sum(nums)/len(nums),2)
+minimum = min(nums)
+maximun = max(nums)
+
+print(f"The biggest number of the list is {maximun}, the smallest is {minimum} and the avarage of the list is {avarage}")
