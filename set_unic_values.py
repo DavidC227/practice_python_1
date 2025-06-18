97b17e876a8246638f6327a95ae1f71028735a6d
David Camacho <ing.dc.pro@gmail.com>
2025-06-17 18:28:24 -0700

    set unic values P34

---
+++ set_unic_values.py
@@ -0,0 +1,5 @@
+repeated_nums = [4,23 ,7,24, 4, 2,23, 9, 3,11,7, 2, 10, 9,4,7,14]
+
+unic_nums = set(repeated_nums) 
+unic_nums_list = list(unic_nums) 
+print(f"Los valores unicos: {unic_nums_list}") 
