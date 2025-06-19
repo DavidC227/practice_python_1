8e04944af26e9202eaf45ebdecaff1706b93df29
David Camacho <ing.dc.pro@gmail.com>
2025-06-18 19:54:42 -0700

    .sort() and sorted() P37

---
+++ sort_sorted.py
@@ -0,0 +1,12 @@
+numbers = [12, 4, 9, 1, 20, 6]
+numbers.sort() 
+print(numbers)
+
+ordered_nums_2 = sorted(numbers) #
+print(ordered_nums_2)
+
+ordered_nums_3 = sorted(numbers, reverse=True) 
+print(ordered_nums_3)
+
+ordered_nums_3.sort(reverse=True)
+print(ordered_nums_3)
