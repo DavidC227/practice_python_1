c0f63bd09018345c421e2ebfed1ad3e2aecb4244
David Camacho <ing.dc.pro@gmail.com>
2025-06-20 18:28:18 -0700

    asterisk pyramid P40

---
+++ asteisk_pyramid.py
@@ -0,0 +1,27 @@
+levels = 5
+
+for i in range(1,levels +1):
+    space = " " * (levels - i)
+    asterisk = "*" * (2 * i - 1)
+    print(space + asterisk)
+
+levels = 5
+
+for i in range(1,levels + 1):
+    space = " " * ((levels*2)-(i + 1))
+    asterisk = "*" * (2 * i - 1)
+    print(space + asterisk)
+
+levels = 5
+
+for i in range(1,levels +1):
+    space = " " * (levels - i)
+    asterisk = "*" * (2 * i - 1)
+    print(space + asterisk)
+
+levels = 5
+
+for i in range(1,levels + 1):
+    space = " " * ((levels*2)-(i + 1))
+    asterisk = "*" * (2 * i - 1)
+    print(space + asterisk)
