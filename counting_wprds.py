0a98a00a06b93aa676258349c95cd71b244b2357
David Camacho <ing.dc.pro@gmail.com>
2025-06-23 00:25:50 -0700

    counting words from a phrase P43

---
+++ counting_wprds.py
@@ -0,0 +1,9 @@
+phrase = "Al raton no le guta el queso" 
+
+count = len(phrase) 
+
+print(f"the number of characters and spaces of the phrase {phrase} are: {count}")
+
+split_words = phrase.split() 
+print(split_words)
+print(f"The total words from the phrase [{phrase}] are: {len(split_words)} words")
