7346c4c49f29c0a3a293030181d974a61f0b8290
David Camacho <ing.dc.pro@gmail.com>
2025-06-26 20:43:58 -0700

    Max frecuencia de caracteres, crear diccionario P49

---
+++ contador_max_dicc_2.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+caracteres = input("ingresa una palabra o frase")
+
+caracteres = caracteres.lower().replace(" ","")
+
+count = Counter(caracteres)
+
+print(count)
+
+llave, valor = count.most_common(1)[0]
+
+print(f"{llave}:{valor}")
